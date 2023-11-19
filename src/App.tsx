4165c18f20c1487463c95f1aaf542d9d253d934e
SaneManiacMG <mmoroane@hotmail.com>
2023-11-19 10:16:23 +0200

    How set event types

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 import { Button } from './components/Button';
+import { Input } from './components/Input';
 
 function App() {
   // const personName = {
@@ -26,6 +27,7 @@ function App() {
   return (
     <div className="App">
       <Button handleClick={(event, id) => console.log("Button cliked: ", event, id)} />
+      <Input value='' handleChange={(event) => console.log("Input event: ", event)} />
     </div>
   );
 }
