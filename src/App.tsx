6f46cc75a0091f111cb68078fa4519531e0d8cae
SaneManiacMG <mmoroane@hotmail.com>
2023-11-20 10:51:08 +0200

    How to use useRef with Dom elements and Mutable data

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
-import User from './components/context/User';
-import { UserContextProvider } from './components/context/UserContext';
+
+import DomRef from './components/ref/DomRef';
+import MutableRef from './components/ref/MutableRef';
 
 
 function App() {
@@ -27,9 +28,8 @@ function App() {
 
   return (
     <div className="App">
-      <UserContextProvider>
-        <User />
-      </UserContextProvider>
+      <DomRef />
+      <MutableRef />
     </div>
   );
 }
